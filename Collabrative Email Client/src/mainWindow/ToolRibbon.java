154acce71b169cc1010b9dae73d01a36453b9b15
Anas Alkhatib <anas.alkhatib@gmail.com>
2013-05-08 20:15:29 -0400

    FIXME: resize icons function
    However the ribbon and buttons do not resize based on the size of the icon
    
    TODO: Figure out why

--- Collabrative Email Client/src/mainWindow/ToolRibbon.java
+++ Collabrative Email Client/src/mainWindow/ToolRibbon.java
@@ -1,8 +1,10 @@
 package mainWindow;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -13,76 +15,94 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
+import net.miginfocom.swing.MigLayout;
 
 public class ToolRibbon extends JFrame implements ActionListener {
 
     JButton deleteButton = new JButton();
     JButton forwardButton = new JButton();
     JButton readButton = new JButton();
-    JButton indoxButton = new JButton();
-    JButton refereshButton = new JButton();
+    JButton inboxButton = new JButton();
+    JButton refreshButton = new JButton();
     JButton composeButton = new JButton();
     JButton replyButton = new JButton();
     mainWindow.ComposeMail obj_2 = new mainWindow.ComposeMail();
 
+    public ImageIcon resizeImageIcon(ImageIcon original) {
+        Image img = original.getImage();
+        BufferedImage bi = new BufferedImage(img.getWidth(null),
+                img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.createGraphics();
+        g.drawImage(img, 0, 0, 64, 64, null);
+        ImageIcon resizedImage = new ImageIcon(bi);
+        return resizedImage;
+    }
+
     public JToolBar toolbar() {
 
         //TODO: resize icons
-        refereshButton.setIcon(new ImageIcon("resources/refresh.png"));
-        refereshButton.setFocusable(false);
-        refereshButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        refereshButton.setVerticalTextPosition(SwingConstants.BOTTOM);
-        refereshButton.setToolTipText("Downloads New Mail");
+        refreshButton.setIcon(resizeImageIcon(new ImageIcon("resources/refresh.png")));
+        refreshButton.setFocusable(false);
+        //refreshButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //refreshButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        refreshButton.setToolTipText("Downloads New Mail");
+        refreshButton.setSize(64, 64);
 
-        composeButton.setIcon(new ImageIcon("resources/compose.png"));
+        composeButton.setIcon(resizeImageIcon(new ImageIcon("resources/compose.png")));
         composeButton.setFocusable(false);
-        composeButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        composeButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        //composeButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //composeButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         composeButton.setToolTipText("Compose Mail");
         composeButton.addActionListener(this);
+        composeButton.setSize(64, 64);
 
-        replyButton.setIcon(new ImageIcon("resources/reply.png"));
+        replyButton.setIcon(resizeImageIcon(new ImageIcon("resources/reply.png")));
         replyButton.setFocusable(false);
-        replyButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        replyButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        //replyButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //replyButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         replyButton.setToolTipText("Reply Mail");
+        replyButton.setSize(64, 64);
 
-        indoxButton.setIcon(new ImageIcon("resources/inbox.png"));
-        indoxButton.setFocusable(false);
-        indoxButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        indoxButton.setVerticalTextPosition(SwingConstants.BOTTOM);
-        indoxButton.setToolTipText("Inbox");
+        inboxButton.setIcon(resizeImageIcon(new ImageIcon("resources/inbox.png")));
+        inboxButton.setFocusable(false);
+        //indoxButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //indoxButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        inboxButton.setToolTipText("Inbox");
+        inboxButton.setSize(64, 64);
 
-        readButton.setIcon(new ImageIcon("resources/unread.png"));
+        readButton.setIcon(resizeImageIcon(new ImageIcon("resources/unread.png")));
         readButton.setFocusable(false);
-        readButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        readButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        //readButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //readButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         readButton.setToolTipText("Unreaded Mail");
+        readButton.setSize(64, 64);
 
-        forwardButton.setIcon(new ImageIcon("resources/forward.png"));
+        forwardButton.setIcon(resizeImageIcon(new ImageIcon("resources/forward.png")));
         forwardButton.setFocusable(false);
-        forwardButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        forwardButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        //forwardButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //forwardButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         forwardButton.setToolTipText("Forward Mail");
+        forwardButton.setSize(64, 64);
 
-        deleteButton.setIcon(new ImageIcon("resources/delete.png"));
+        deleteButton.setIcon(resizeImageIcon(new ImageIcon("resources/delete.png")));
         deleteButton.setFocusable(false);
-        deleteButton.setHorizontalTextPosition(SwingConstants.CENTER);
-        deleteButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        //deleteButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        //deleteButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         deleteButton.setToolTipText("Delete Mail");
-
+        deleteButton.setSize(64, 64);
 
         JToolBar tool_bar = new javax.swing.JToolBar();
+        //tool_bar.setLayout((new MigLayout()));
         tool_bar.setRollover(true);
-        tool_bar.add(refereshButton);
+        tool_bar.add(refreshButton);
         tool_bar.add(composeButton);
         tool_bar.add(replyButton);
-        tool_bar.add(indoxButton);
+        tool_bar.add(inboxButton);
         tool_bar.add(readButton);
         tool_bar.add(forwardButton);
-
         tool_bar.add(deleteButton);
         return tool_bar;
     }
