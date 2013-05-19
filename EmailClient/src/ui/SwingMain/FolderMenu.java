ff94c09d1cf9f729a487a04e13ef176972dc3095
Eric Chan <chanman_ca@yahoo.com>
2013-05-19 14:37:43 -0400

    Folder menu

---
+++ EmailClient/src/ui/SwingMain/FolderMenu.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.SwingMain;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.tree.TreeNode;
+
+/**
+ *
+ * @author chanman
+ */
+public class FolderMenu extends JPopupMenu {
+
+    TreeNode selected;
+
+    FolderMenu(TreeNode last) {
+        selected = last;
+
+        JMenuItem deleteFolder = new JMenuItem("Delete");
+        JMenuItem newFolder = new JMenuItem("New Folder");
+        JMenuItem moveFolder = new JMenuItem("Move");
+
+        deleteFolder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                deleteFolder();
+            }
+        });
+
+        newFolder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                newFolder();
+            }
+        });
+
+        moveFolder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                moveFolder();
+            }
+        });
+
+        this.add(deleteFolder);
+        this.add(newFolder);
+        this.add(moveFolder);
+    }
+
+    private void deleteFolder() {
+        // selected -> folderID
+        // controller.delete(folderIO)
+        // update tree
+
+        JOptionPane.showMessageDialog(null, "Delete " + selected.toString(), null, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void moveFolder() {
+        // get destination (a foldeid
+        // selected -> folderID
+        // controller.move(...)
+        // update tree
+
+        JOptionPane.showMessageDialog(null, "Move", null, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void newFolder() {
+        // get name
+        // selected -> folderID
+        // controller.newfolder(folderIO,name)
+        // update tree
+
+        JOptionPane.showMessageDialog(null, "New", null, JOptionPane.ERROR_MESSAGE);
+    }
+}
