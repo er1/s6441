87fafde62a2a8b44f19c5cca8093bc6bb3818f7e
Anas Alkhatib <anas.alkhatib@gmail.com>
2013-05-19 14:08:40 -0400

    loadEmailList tesT

--- EmailClient/test/Persist/FileSystemStorageTest.java
+++ EmailClient/test/Persist/FileSystemStorageTest.java
@@ -98,8 +98,6 @@ public class FileSystemStorageTest {
         String[] expResult = null;
         String[] result = instance.loadMessageListFromFolder(folder);
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
