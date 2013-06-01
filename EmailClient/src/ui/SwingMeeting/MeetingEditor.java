5830d4714f2ad65d92d6899a769a4ce95b36a704
Eric Chan <chanman_ca@yahoo.com>
2013-06-01 10:01:24 -0400

    added meeting window to populate

---
+++ EmailClient/src/ui/SwingMeeting/MeetingEditor.java
@@ -0,0 +1,20 @@
+package ui.SwingMeeting;
+
+import javax.swing.JFrame;
+
+/**
+ * Edit Meeting Window
+ */
+public class MeetingEditor extends JFrame {
+
+    String messageId;
+
+    public MeetingEditor(String messageId) {
+        super("Meeting");
+        this.messageId = messageId;
+    }
+    
+    public void init() {
+        
+    }
+}
