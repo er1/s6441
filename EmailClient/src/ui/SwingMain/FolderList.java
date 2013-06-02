f9d96f179979fc5d713fd21cb15c84323127cd3d
Eric Chan <er_cha@cs.concordia.ca>
2013-06-02 15:18:26 -0400

    extracted folder tree model

--- EmailClient/src/ui/SwingMain/FolderList.java
+++ EmailClient/src/ui/SwingMain/FolderList.java
@@ -6,7 +6,6 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPopupMenu;
@@ -14,10 +13,8 @@ import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
-import javax.swing.event.TreeModelListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
 /**
@@ -29,88 +26,6 @@ public class FolderList extends JTree {
     MessageController store;
     MessageList list;
     FolderTreeModel model;
-
-    private class FolderNode {
-
-        String id;
-        String name;
-
-        public FolderNode(MessageController controller, String id) {
-            this.id = id;
-            this.name = controller.getFolderName(id);
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public String toString() {
-            return getName();
-        }
-    }
-
-    private class FolderTreeModel implements TreeModel {
-
-        MessageController controller;
-
-        public FolderTreeModel(MessageController store) {
-            controller = store;
-        }
-
-        @Override
-        public Object getRoot() {
-            return new FolderNode(controller, controller.getRootFolderId());
-        }
-
-        @Override
-        public Object getChild(Object folderid, int index) {
-            String id = ((FolderNode) folderid).getId();
-            String[] folders = controller.getFolderList(id);
-            return new FolderNode(controller, folders[index]);
-        }
-
-        @Override
-        public int getChildCount(Object folderid) {
-            String id = ((FolderNode) folderid).getId();
-            String[] folders = controller.getFolderList(id);
-            return folders.length;
-        }
-
-        @Override
-        public boolean isLeaf(Object o) {
-            return false;
-        }
-
-        @Override
-        public void valueForPathChanged(TreePath tp, Object o) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public int getIndexOfChild(Object parent, Object child) {
-            String parentid = ((FolderNode) parent).getId();
-            String childid = ((FolderNode) child).getId();
-            String[] folders = controller.getFolderList(parentid);
-            return Arrays.asList(folders).indexOf(childid);
-        }
-
-        @Override
-        public void addTreeModelListener(TreeModelListener tl) {
-            // FIXME
-            //throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public void removeTreeModelListener(TreeModelListener tl) {
-            // FIXME
-            //throw new UnsupportedOperationException("Not supported yet.");
-        }
-    }
 
     /**
      * constructor
@@ -176,7 +91,7 @@ public class FolderList extends JTree {
 
     void makeMenu(Point mouseposition) {
         TreePath path = this.getPathForLocation(mouseposition.x, mouseposition.y);
-        FolderNode temp = (FolderNode) path.getLastPathComponent();
+        FolderTreeModel.FolderNode temp = (FolderTreeModel.FolderNode) path.getLastPathComponent();
         String last = temp.getId();
 
         JPopupMenu menu = new FolderMenu(last);
@@ -194,7 +109,7 @@ public class FolderList extends JTree {
     private void changefolder() {
         try {
             TreePath path = this.getSelectionPath();
-            String id = ((FolderNode) path.getLastPathComponent()).getId();
+            String id = ((FolderTreeModel.FolderNode) path.getLastPathComponent()).getId();
             list.displayFolder(id);
         } catch (NullPointerException e) {
             // FIXME
---
+++ EmailClient/src/ui/SwingMain/FolderTreeModel.java
@@ -0,0 +1,89 @@
+package ui.SwingMain;
+
+import Email.MessageController;
+import java.util.Arrays;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+class FolderTreeModel implements TreeModel {
+
+    MessageController controller;
+
+    public class FolderNode {
+
+        String id;
+        String name;
+
+        public FolderNode(MessageController controller, String id) {
+            this.id = id;
+            this.name = controller.getFolderName(id);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return getName();
+        }
+    }
+
+    public FolderTreeModel(MessageController store) {
+        controller = store;
+    }
+
+    @Override
+    public Object getRoot() {
+        return new FolderTreeModel.FolderNode(controller, controller.getRootFolderId());
+    }
+
+    @Override
+    public Object getChild(Object folderid, int index) {
+        String id = ((FolderTreeModel.FolderNode) folderid).getId();
+        String[] folders = controller.getFolderList(id);
+        return new FolderTreeModel.FolderNode(controller, folders[index]);
+    }
+
+    @Override
+    public int getChildCount(Object folderid) {
+        String id = ((FolderTreeModel.FolderNode) folderid).getId();
+        String[] folders = controller.getFolderList(id);
+        return folders.length;
+    }
+
+    @Override
+    public boolean isLeaf(Object o) {
+        return false;
+    }
+
+    @Override
+    public void valueForPathChanged(TreePath tp, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getIndexOfChild(Object parent, Object child) {
+        String parentid = ((FolderTreeModel.FolderNode) parent).getId();
+        String childid = ((FolderTreeModel.FolderNode) child).getId();
+        String[] folders = controller.getFolderList(parentid);
+        return Arrays.asList(folders).indexOf(childid);
+    }
+
+    @Override
+    public void addTreeModelListener(TreeModelListener tl) {
+        // FIXME
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void removeTreeModelListener(TreeModelListener tl) {
+        // FIXME
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
