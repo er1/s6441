8df675cbf3ffc9de6e0659f66740dacca406c481
karthik krishnan <nkkrish21@gmail.com>
2013-06-03 00:07:27 -0400

    Search Bar File

---
+++ EmailClient/src/ui/SwingMain/searchBar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.SwingMain;
+
+import ui.LabeledTextField;
+
+/**
+ *
+ * @author KarthikKrishnan
+ */
+public class searchBar {
+
+    
+}
